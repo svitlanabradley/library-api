cab30734481e52dadbc0010c9c64bf16902bf892
svitlanabradley <svitlana.bradley@gmail.com>
2025-11-17 12:11:53 +0100

    corrected

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });

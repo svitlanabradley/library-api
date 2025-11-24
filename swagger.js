25a87a843ecbfd3f0c08149f3d3e631cf7fd7570
svitlanabradley <svitlana.bradley@gmail.com>
2025-11-24 16:36:08 +0100

    corrected

--- swagger.js
+++ swagger.js
@@ -26,30 +26,3 @@ const options = {
 
 const specs = swaggerJsdoc(options);
 module.exports = specs;
-
-
-
-
-
-
-// const swaggerJsdoc = require('swagger-jsdoc');
-
-// const options = {
-//   definition: {
-//     openapi: "3.0.0", // Swagger version
-//     info: {
-//       title: "Library API",
-//       version: "1.0.0",
-//       description: "API for Books and Users",
-//     },
-//     servers: [
-//       {
-//         url: "https://library-api-gmet.onrender.com",
-//       },
-//     ],
-//   },
-//   apis: ["./routes/*.js"], 
-// };
-
-// const specs = swaggerJsdoc(options);
-// module.exports = specs;
